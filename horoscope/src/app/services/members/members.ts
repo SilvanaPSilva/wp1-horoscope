774434f690b27fd137fc999efe5fb249fe937736
SilvanaPSilva <sil.pereiradasilva@gmail.com>
2026-05-12 10:11:55 +0100

    refactor: add InterfaceMember typing to getMembers() and clean Members service

--- horoscope/src/app/services/members/members.ts
+++ horoscope/src/app/services/members/members.ts
@@ -1,30 +1,30 @@
-import { HttpClient } from '@angular/common/http'; //vadded in horoscope/src/app/services/horoscope/horoscope.ts
+import { HttpClient } from '@angular/common/http'; // HttpClient used to communicate with backend API
 import { Injectable } from '@angular/core';
+import { InterfaceMember } from '../../models/interfaceMember';
+
 
 @Injectable({
   providedIn: 'root',
 })
 
-//
 export class Members {
-
-  //Inject the HttpClient to make HTTP requests to the backend API
-  constructor (private http: HttpClient) {
-  }
-
+  
   private apiUrl = 'http://localhost:3000/members';
 
-  //Method to fetch the list of members from the backend API (routes/membersRoutes) using an HTTP GET request
+  //Inject the HttpClient to make HTTP requests to the backend API
+  constructor(private http: HttpClient) {}
+
+  //*
   getMembers() {
-    return this.http.get(this.apiUrl);
+    return this.http.get<InterfaceMember[]>(this.apiUrl);
   }
 
-  addMember(member: any){
-    return this.http.post(this.apiUrl, member);
+  addMember(member: InterfaceMember) {
+    return this.http.post<InterfaceMember>(this.apiUrl, member);
   }
 
   deleteMember(id: string) {
-    return this.http.delete(this.apiUrl+'/'+id);
+    return this.http.delete<{ message: string }>(this.apiUrl + '/' + id);
   }
 }
 
